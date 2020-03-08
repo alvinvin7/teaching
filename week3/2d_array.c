b818a7c72a15f712e188a8e777465ef4a54ef5c8
Alvin Fujito <a.fujito@unsw.edu.au>
2020-03-08 16:46:12 +1100

    finalised

--- week3/2d_array.c
+++ week3/2d_array.c
@@ -1,8 +1,37 @@
+// Demonstrating how nested loop works
+// 
+// Example:
+//
+// Size: 3
+// (0, 0) (0, 1) (0, 2)
+// (1, 0) (1, 1) (1, 2)
+// (2, 0) (2, 1) (2, 2)
+// 
+// Made by Alvin Fujito
+// Date: March 2020
+// Contact: a.fujito@unsw.edu.au
+
 #include <stdio.h>
 
 int main (void) {
 
+    int size;
+    printf("Size: ");
+    scanf("%d", &size);
 
+    int row = 0;
+    int col = 0;
+
+    while (row < size) {
+        col = 0;
+        while (col < size) {
+            printf("(%d, %d) ", row, col);
+            col++;
+        }
+        printf("\n");
+        row++;
+    }
 
     return 0;
 }
+
